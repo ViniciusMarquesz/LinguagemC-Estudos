411d77a4b4b5fa3641f76de7400a2e33d8837a9f
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-02-23 14:58:43 -0300

    Ponteiros

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+
+    //Variavel
+
+    int a = 20;
+
+    //Imprimindo o valor de uma variável
+    printf("Valor de a: %d \n", a);
+
+    //Imprimindo o endereço de uma variável
+    printf("Valor de a: %d \n", &a);
+
+    //Variaveis armazenam valores
+    int b = 10;
+
+    //Ponteiros  armazenam posições da memoria
+    int *ponteiro;
+
+    //Ponteiro recebendo posição na memoria da variavel B
+    ponteiro = &b;
+
+    //Imprimindo o valor de uma variável
+    printf("Valor de a: %d \n", b);
+
+    //* Pode ser lido como "Conteudo apontado por"
+    *ponteiro = 40;
+
+    //Imprimindo o valor de uma variável
+    printf("Valor de a: %d \n", b);
+
+
     return 0;
+
 }
